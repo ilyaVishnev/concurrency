ce8b024faf1886702eb954a01176bd57e22d38b3
ilyaVishnev <dong85@mail.ru>
2025-02-13 17:26:22 +0300

    Async

--- src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
@@ -1,24 +1,43 @@
 package course.concurrency.m2_async.minPrice;
 
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PriceAggregator {
 
-    private PriceRetriever priceRetriever = new PriceRetriever();
+    public static final List<Double> mynext = new ArrayList<>(Arrays.asList(Double.NaN));
+
+    private static PriceRetriever priceRetriever = new PriceRetriever();
+
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
     }
 
-    private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
+    private static Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
     }
 
     public double getMinPrice(long itemId) {
-        // place for your code
-        return 0;
+        List<CompletableFuture<Double>>shops= shopIds.stream()
+                .map(id -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, id),executor)
+                        .completeOnTimeout(Double.POSITIVE_INFINITY, 2900l, TimeUnit.MILLISECONDS)
+                        .exceptionally(ex->Double.POSITIVE_INFINITY)
+                )
+                .collect(Collectors.toList());
+
+        CompletableFuture.allOf(shops.toArray(CompletableFuture[]::new)).join();
+
+        return shops.stream().mapToDouble(CompletableFuture::join).filter(Double::isFinite)
+                .min().orElse(Double.NaN);
+
     }
 }
--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -3,24 +3,44 @@ package course.concurrency.m2_async.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+
+import java.beans.Transient;
+import java.util.concurrent.Executor;
 
 @Component
 public class AsyncClassTest {
 
     @Autowired
+    @Lazy
+    private AsyncClassTest test;
+
+    @Autowired
     public ApplicationContext context;
 
     @Autowired
-    @Qualifier("applicationTaskExecutor")
-    private ThreadPoolTaskExecutor executor;
+    /*@Qualifier("applicationTaskExecutor")*/
+    @Qualifier("taskExecutor")
+    private Executor executor;
 
+   /* @Autowired
+    *//*@Qualifier("applicationTaskExecutor")*//*
+    @Qualifier("secondTaskExecutor")
+    private Executor second_executor;*/
+
+    @Async("taskExecutor")
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
+        test.internalTask();
     }
 
+    @Async("taskExecutor")
     public void internalTask() {
         System.out.println("internalTask: " + Thread.currentThread().getName());
     }
